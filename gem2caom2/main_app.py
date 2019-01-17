5467fa1b810e72a4ebaf1ae61a18a49d410cd0cb
Jeff Burke <Jeff.Burke@nrc-cnrc.gc.ca>
2019-01-17 08:14:50 -0800

    s2378 - removed unused function

--- gem2caom2/main_app.py
+++ gem2caom2/main_app.py
@@ -100,7 +100,6 @@ import traceback
 import requests
 from requests.adapters import HTTPAdapter
 from urllib3 import Retry
-import re
 
 from caom2 import Observation
 from caom2utils import ObsBlueprint, get_gen_proc_arg_parser, gen_proc
@@ -221,30 +220,6 @@ class GemName(ec.StorageName):
     @staticmethod
     def is_preview(entry):
         return '.jpg' in entry
-
-
-def multi_replace(string, replacements):
-    """
-    Given a string and a replacement map, it returns the replaced string.
-
-    :param str string: string to execute replacements on
-    :param dict replacements: replacement dictionary {value to find: value
-                                                      to replace}
-    :rtype: str
-
-    """
-    # Place longer ones first to keep shorter substrings from matching where
-    # the longer ones should take place
-    # For instance given the replacements {'ab': 'AB', 'abc': 'ABC'}
-    # against the string 'hey abc', it should produce
-    # 'hey ABC' and not 'hey ABc'
-    substrs = sorted(replacements, key=len, reverse=True)
-
-    # Create a big OR regex that matches any of the substrings to replace
-    regexp = re.compile('|'.join(map(re.escape, substrs)))
-
-    # For each match, look up the new string in the replacements
-    return regexp.sub(lambda match: replacements[match.group(0)], string)
 
 
 def get_obs_metadata(obs_id):
