896145fa5d02e924f406182106627a3373a87580
SharonGoliath <goliaths.cadc@gmail.com>
2019-06-26 12:44:34 -0700

    s2482 - make operational.

--- scripts/gem_run_query.sh
+++ scripts/gem_run_query.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# CONTAINER="bucket.canfar.net/gem2caom2"
-CONTAINER="gem_run_int"
+CONTAINER="bucket.canfar.net/gem2caom2"
 
 echo "Get a proxy certificate"
 cp $HOME/.ssl/cadcproxy.pem ./ || exit $?
@@ -10,7 +9,7 @@ echo "Get the container"
 docker pull ${CONTAINER} || exit $?
 
 echo "Run gem_run container"
-docker run --rm --name gem_run_query -v ${PWD}:/usr/src/app/ ${CONTAINER} gem_run_query || exit $?
+docker run -m=7g --rm --name gem_run_query -v ${PWD}:/usr/src/app/ ${CONTAINER} gem_run_query || exit $?
 
 date
 exit 0
