3ebea8c6e8b79d31d5ffdf0b20a26e5abc120609
SharonGoliath <Sharon.Goliath@nrc-cnrc.gc.ca>
2020-08-15 11:08:56 -0700

    Update gem_run.sh
    Use bucket as the image location.

--- scripts/gem_run.sh
+++ scripts/gem_run.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# IMAGE="bucket.canfar.net/gem2caom2"
-IMAGE="gem_build_test"
+IMAGE="bucket.canfar.net/gem2caom2"
 
 echo "Get a proxy certificate"
 cp $HOME/.ssl/cadcproxy.pem ./ || exit $?
 
 echo "Get image ${IMAGE}"
-# docker pull ${IMAGE} || exit $?
+docker pull ${IMAGE} || exit $?
 
 echo "Run image ${IMAGE}"
 docker run -m=7g --rm --name gem_run_builder -v ${PWD}:/usr/src/app/ ${IMAGE} gem_run || exit $?
