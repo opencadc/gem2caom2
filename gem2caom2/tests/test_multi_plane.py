c4861c942975a94241a40f02ddcae72ea1dbf237
Sharon Goliath <Sharon.Goliath@nrc-cnrc.gc.ca>
2020-09-01 16:24:58 -0700

    s2748 - Add support for ALOPEKE/ZORRO observationID handling to GemName.

--- gem2caom2/tests/test_multi_plane.py
+++ gem2caom2/tests/test_multi_plane.py
@@ -166,7 +166,7 @@ def test_multi_plane(tap_mock, test_name):
 
 def _get_lineage(obs_id):
     result = ''
-    if obs_id == 'GN-2020A-Q-132-0-0':
+    if obs_id in ['GN-2020A-Q-132-0-0', 'N20191219A0004']:
         product_id = LOOKUP[obs_id][0][:-1]
         x = mc.get_lineage(gem_name.ARCHIVE, product_id,
                            f'{LOOKUP[obs_id][0]}.fits', gem_name.SCHEME)
